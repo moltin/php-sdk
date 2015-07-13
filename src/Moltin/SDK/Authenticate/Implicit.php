d91a90418db4e4ff80a356022f20ed07db077472
jHoldroyd <jamie@molt.in>
2015-07-14 00:13:03 +0100

    Adding implicit grant type

---
+++ src/Moltin/SDK/Authenticate/Implicit.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+* This file is part of Moltin PHP-SDK, a PHP package which
+* provides convinient and rapid access to the API.
+*
+* Copyright (c) 2013 Moltin Ltd.
+* http://github.com/moltin/php-sdk
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*
+* @package moltin/php-sdk
+* @author Jamie Holdroyd <jamie@molt.in>
+* @copyright 2013 Moltin Ltd.
+* @version dev
+* @link http://github.com/moltin/php-sdk
+*
+*/
+
+namespace Moltin\SDK\Authenticate;
+
+use Moltin\SDK\Exception\InvalidResponseException as InvalidResponse;
+
+class Implicit implements \Moltin\SDK\AuthenticateInterface
+{
+    protected $data = array(
+        'token'   => null,
+        'refresh' => null,
+        'expires' => null
+    );
+
+    public function authenticate($args, \Moltin\SDK\SDK $parent)
+    {
+        // Variables
+        $url  = $parent->url . 'oauth/access_token';
+        $data = array(
+            'grant_type' => 'implicit',
+            'client_id'  => $args['client_id']
+        );
+
+        $parent->request->setup($url, 'POST', $data);
+        $result = $parent->request->make();
+
+        // Check response
+        $result = json_decode($result, true);
+
+        // Check JSON for error
+        if (isset($result['error'])) {
+            throw new InvalidResponse($result['error']);
+        }
+
+        // Set data
+        $this->data['token']   = $result['access_token'];
+        $this->data['refresh'] = $result['refresh_token'];
+        $this->data['expires'] = $result['expires'];
+    }
+
+    public function get($key)
+    {
+        if ( ! isset($this->data[$key])) {
+            return;
+        }
+
+        return $this->data[$key];
+    }
+}
