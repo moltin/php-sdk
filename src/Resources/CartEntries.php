9984bb93d622d1bf9f58d596474479ab69c0f95e
Yusuf Hasan Miyan <yusufh@protonmail.com>
2018-04-26 09:08:53 +1000

    Added the CartEntries Resource.

---
+++ src/Resources/CartEntries.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Moltin\Resources;
+
+use Moltin\Resource as Resource;
+
+/**
+ * Provides the CartEntries Resource.
+ */
+class CartEntries extends Resource {
+  public $uri = 'flows/carts/entries';
+
+}
